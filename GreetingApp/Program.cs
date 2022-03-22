f73081e17bfe60516465fa58d61c97ff03fbf0cb
Danny Warren <danny@theforbiddenarts.com>
2022-03-22 14:25:43 -0400

    Prep work space for coding

--- GreetingApp/Program.cs
+++ GreetingApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// Welcome User to App
+
+
+// Ask for First Name
+
+
+// Greet user by name
+
