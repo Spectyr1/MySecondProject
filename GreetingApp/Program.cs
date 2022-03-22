12e1b4b79f6dc14f2a92d0c257b111923c32e9c8
Danny Warren <danny@theforbiddenarts.com>
2022-03-22 14:53:26 -0400

    Added code to application

--- GreetingApp/Program.cs
+++ GreetingApp/Program.cs
@@ -1,10 +1,19 @@
 ﻿
 
 // Welcome User to App
-
+Console.WriteLine("Welcome to the Greeting Application!");
+Console.WriteLine("This Application was built by Danny Warren");
+Console.WriteLine("------------------------------------------");
+Console.WriteLine();
 
 // Ask for First Name
-
+string firstName;
+Console.Write("Please enter your First Name: ");
+firstName = Console.ReadLine();
 
 // Greet user by name
+Console.WriteLine();
+Console.WriteLine($"hello {firstName}");
 
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
